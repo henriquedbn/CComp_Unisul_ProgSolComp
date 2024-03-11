b251fc5b438a0a765c5ba6a8f23091871d61f835
henriquedbn <henriquenoronha@gmail.com>
2024-03-11 12:02:14 -0300

    Inserção dos requisitos do projeto em comentário na Classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-/** Atividade da unidade "Pratique" */
+/** Atividade da unidade "Pratique" -
+ * Atributos necessários:
+ * -> Eventos: nome, endereço, categoria (festas, eventos esportivos, shows, entre outros exemplos), horário e descrição;
+ * -> Usuários: nome, documento, cidade.
+ * Funcionalidades necessárias:
+ *  -> Prover um espaço para cadastro do usuário;
+ * -> Consultar os eventos cadastrados e decidir participar de qualquer um que esteja listado;
+ * -> Cadastrar eventos, definindo os atributos;
+ * -> Através do horário, o programa deve ordenar os eventos mais próximos e informar se um evento está ocorrendo no momento (é desejável utilizar a estrutura DateTime para o controle de horários);
+ * -> Informar os eventos que já ocorreram;
+ * -> informações dos eventos devem ser salvas em um arquivo de texto chamado events.data;
+ * -> Toda vez que o programa for aberto, deve carregar os eventos a partir da leitura deste arquivo.
+ * */
 
 public class Main {
     public static void main(String[] args) {
